05ab54a80d93d3a7b7de9e33f10cfaf540c0c782
Francis <francis@aspgems.com>
2012-07-03 18:08:02 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeleton::Application.config.secret_token = '399c494e638e923fc91c9153af4af5bf7e1988c383eb2fc57e64ea450d85f217630ab071ec59583efde196ec01ed06e4cde19d53a02d00f2abe93aeb6bfcc538'
